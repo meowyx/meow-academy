24d076b26d55bcda26883a55d7cdbbae9c660437
meowyx <qafka256@gmail.com>
2023-01-14 09:39:12 +0600

    changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className="hero-content flex-col lg:flex-row-reverse">
       <img src="https://i.imgur.com/fXv9uGJ.jpg" className="max-w-3xl rounded-lg shadow-2xl" />
       <div>
-        <p className="py-6 text-5xl font-serif"> <b className='underline decoration-wavy decoration-yellow-400'>🐱 Academy</b> is an open source web3 learning resource with easy to understand contents.</p>
+        <p className="py-6 text-5xl font-serif"> <b className='underline decoration-wavy decoration-yellow-400'>🐱🌱</b> is my garden that will keep growing..</p>
         <Link href={'https://academy-lessons.vercel.app/'}>
       <button className="btn btn-block btn-primary">Get Started</button>
       </Link>
