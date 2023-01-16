64cc5e002fce9733946ce2864c0dfde55d675626
meowyx <qafka256@gmail.com>
2023-01-16 10:05:55 +0600

    lines

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,16 +5,16 @@ import Link from 'next/link'
 const Hero = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-      <img src="https://i.imgur.com/fXv9uGJ.jpg" className="max-w-3xl rounded-lg shadow-2xl" />
-      <div>
-        <p className="py-6 text-5xl font-serif"> <b className='underline decoration-wavy decoration-yellow-400'>🐱 Academy</b> is a open source learning resource for newcomers.</p>
-        <Link href={'https://academy-lessons.vercel.app/'}>
-      <button className="btn btn-block btn-primary">Get Started</button>
-      </Link>
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src="https://i.imgur.com/fXv9uGJ.jpg" className="max-w-3xl rounded-lg shadow-2xl" />
+        <div>
+          <p className="py-6 text-5xl font-serif"> <b className='underline decoration-wavy decoration-yellow-400'>🐱 Academy</b> is an open source web3 learning resource with easy to understand contents.</p>
+          <Link href={'https://academy-lessons.vercel.app/'}>
+            <button className="btn btn-block btn-primary">Get Started</button>
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
