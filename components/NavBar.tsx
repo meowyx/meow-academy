f46ba242976d2dc663691e16b5437c670acaa777
meowyx <qafka256@gmail.com>
2023-01-16 10:01:20 +0600

    added something

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const NavBar = () => {
   return (
     <div className="navbar bg-base-100">
-  <a className="btn btn-ghost normal-case glass text-4xl">🐱🌱</a>
+  <a className="btn btn-ghost normal-case glass text-4xl">🐱 Academy</a>
 </div>
   )
 }
